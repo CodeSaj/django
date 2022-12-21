9d753acc1981c48318382b269473df5239ee7cdc
Ryan Tran <90876903+ryant269@users.noreply.github.com>
2022-12-20 23:59:48 -0800

    Update cart.py

--- Project/netsetshop/cart/cart.py
+++ Project/netsetshop/cart/cart.py
@@ -11,10 +11,8 @@ class Cart(object):
         self.session = request.session
         cart = self.sessions.get(settings.CART_SESSION_ID)
         if not cart:
-            cart = self.session[settings.CART_SESSION_ID]
-            if not cart:
-                cart = self.session[settings.CART_SESSION_ID] = {}
-            self.cart = cart
+            cart = self.session[settings.CART_SESSION_ID] = {}
+        self.cart = cart
 
     def add(self, product, quantity=1, override_quantity=False):
         product_id = str(product.id)
